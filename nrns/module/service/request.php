73f4e5c8d320d32ee2ef39bd6314c26aaba0c2ac
platdesign <christianblaschke@Christians-Mac-mini.local>
2013-12-15 01:42:19 +0100

    debug

--- nrns/module/service/request.php
+++ nrns/module/service/request.php
@@ -58,14 +58,7 @@
 		 * @author Christian Blaschke
 		 */
 		public function cached_getBase() {
-			
 			return $this->getProtocol()."://".$_SERVER['SERVER_NAME'].$this->getScriptPath();
-			
-			
-			$fullUrl 	= $this->getUrl();
-			$scriptpath = $this->getScriptPath();
-			return rtrim(substr($fullUrl, 0, strrpos($fullUrl, $scriptpath)).$scriptpath, "/");
-			
 		}
 	
 	
